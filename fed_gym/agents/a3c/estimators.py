688b2efe34be9570a04b37e669994423df1b7eaa
Allen Tran <realallentran@gmail.com>
2018-01-07 11:32:29 -0800

    gated estimator + tests

--- fed_gym/agents/a3c/estimators.py
+++ fed_gym/agents/a3c/estimators.py
@@ -35,6 +35,109 @@ def rnn_graph_lstm(inputs, hidden_size, num_layers, is_training):
     return state[-1]
 
 
+class DiscreteAndContPolicyEstimator():
+    """
+    Policy Function approximator. Given a observation, returns probabilities
+    over all possible actions.
+
+    Args:
+      num_discrete: Size of the action space.
+      input_shape: List of input shape, batch size is leading dimension
+      temporal_input_shape: List of temporal_input shape, batch size is leading dimension
+      reuse: If true, an existing shared network will be re-used.
+      trainable: If true we add train ops to the network.
+        Actor threads that don't update their local models and don't need
+        train ops would set this to false.
+    """
+
+    BUY_IDX = 1
+    SELL_IDX = 2
+
+    def __init__(self, num_discrete, static_size, temporal_size, shared_layer, static_hidden_size=64, reuse=False, trainable=True, learning_rate=1e-4):
+
+        self.states = tf.placeholder(shape=(None, static_size), dtype=tf.float32, name="X")
+        self.history = tf.placeholder(shape=(None, None, temporal_size), dtype=tf.float32, name="X_t")
+        self.advantages = tf.placeholder(shape=(None,), dtype=tf.float32, name='advantages')
+
+        # binary one-hot float32 for actions
+        self.discrete_actions = tf.placeholder(shape=(None, num_discrete), dtype=tf.float32, name="discrete_actions")
+        # Note: if actions are transformed, they should be provided in the untransformed shape (i.e N(mu, sig^2) space)
+        self.actions = tf.placeholder(shape=(None, ), dtype=tf.float32, name="cont_actions")
+
+        self.num_actions = num_discrete
+        self.static_size = static_size
+        self.temporal_size = temporal_size
+
+        X = tf.to_float(self.states)
+        X_t = tf.to_float(self.history)
+
+        with tf.variable_scope("policy_net"):
+            state_T = shared_layer(X_t)
+            dense_temporal = dense_layers(state_T)
+            dense_static = tf.contrib.layers.fully_connected(X, static_hidden_size * 2)
+            dense_static = tf.contrib.layers.fully_connected(dense_static, static_hidden_size)
+            dense_output = tf.concat([dense_temporal, dense_static], axis=-1)
+
+            class_hidden = tf.contrib.layers.fully_connected(dense_output, static_hidden_size * 2)
+            class_hidden = tf.contrib.layers.fully_connected(class_hidden, static_hidden_size)
+            discrete_probs = tf.contrib.layers.fully_connected(class_hidden, num_discrete, activation_fn=tf.nn.softmax)
+
+            normal_params = tf.contrib.layers.fully_connected(dense_output, static_hidden_size * 2)
+            normal_params = tf.contrib.layers.fully_connected(normal_params, static_hidden_size)
+            normal_params = tf.contrib.layers.fully_connected(normal_params, num_discrete * 2, activation_fn=None)
+            normal_params = tf.reshape(normal_params, [-1, num_discrete, 2])
+            mu = normal_params[:, :, 0]
+            sigma = tf.nn.softplus(normal_params[:, :, 1]) + keras.backend.epsilon()
+
+            self.predictions = {
+                "mu": mu,
+                "sigma": sigma,
+                "probs": discrete_probs
+            }
+
+            discrete_entropy = - tf.reduce_sum(discrete_probs * tf.log(discrete_probs + keras.backend.epsilon()), axis=-1)
+            cont_entropy = tf.reduce_mean(0.5 * (tf.log(2 * math.pi * tf.square(sigma) + keras.backend.epsilon()) + 1), axis=-1)
+            self.entropy_mean = tf.reduce_mean(cont_entropy + discrete_entropy, name="entropy_mean")
+
+            action_probs = tf.reduce_sum(discrete_probs * self.discrete_actions, axis=-1)
+            mu_action = tf.reduce_sum(mu * self.discrete_actions, axis=-1)
+            sig_action = tf.reduce_sum(sigma * self.discrete_actions, axis=-1)
+
+            nll_discrete = - tf.log(action_probs)
+            nll_cont = tf.log(sig_action + keras.backend.epsilon()) + tf.square(self.actions - mu_action) / (2 * tf.square(sig_action))
+            loss = (nll_discrete + nll_cont) * self.advantages[:, None]
+
+            self.loss = tf.identity(tf.reduce_sum(loss) - 1e-3 * self.entropy_mean, name='loss')
+
+            tf.summary.scalar(self.loss.op.name, self.loss)
+            tf.summary.scalar(self.entropy_mean.op.name, self.entropy_mean)
+            tf.summary.histogram('cont_entropy', cont_entropy)
+            tf.summary.histogram('discrete_entropy', discrete_entropy)
+            tf.summary.histogram('nll', nll_cont + nll_discrete)
+            tf.summary.histogram('advantages', self.advantages)
+            tf.summary.histogram('buy_prob', discrete_probs[:, self.BUY_IDX])
+            tf.summary.histogram('sell_prob', discrete_probs[:, self.SELL_IDX])
+            tf.summary.histogram('sigmoid_actions', tf.nn.sigmoid(self.actions))
+            tf.summary.histogram('tanh_actions', tf.nn.tanh(self.actions))
+            tf.summary.histogram('mu', mu)
+            tf.summary.histogram('sigma', sigma)
+
+            if trainable:
+                self.optimizer = tf.train.AdamOptimizer(learning_rate)
+                self.grads_and_vars = self.optimizer.compute_gradients(self.loss)
+                self.grads_and_vars = [[grad, var] for grad, var in self.grads_and_vars if grad is not None]
+                self.train_op = self.optimizer.apply_gradients(
+                    self.grads_and_vars,
+                    global_step=tf.train.get_global_step()
+                )
+
+        # Merge summaries from this network and the shared network (but not the value net)
+        var_scope_name = tf.get_variable_scope().name
+        summary_ops = tf.get_collection(tf.GraphKeys.SUMMARIES)
+        sumaries = [s for s in summary_ops if var_scope_name in s.name]
+        self.summaries = tf.summary.merge(sumaries)
+
+
 class GaussianPolicyEstimator():
     """
     Policy Function approximator. Given a observation, returns probabilities
--- tests/estimators_tests.py
+++ tests/estimators_tests.py
@@ -1,7 +1,98 @@
 import numpy as np
 import tensorflow as tf
 
-from fed_gym.agents.a3c.estimators import GaussianPolicyEstimator, ValueEstimator, rnn_graph_lstm
+from fed_gym.agents.a3c.estimators import GaussianPolicyEstimator, ValueEstimator, rnn_graph_lstm, DiscreteAndContPolicyEstimator
+
+
+class GatedPolicyEstimatorTest(tf.test.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(GatedPolicyEstimatorTest, cls).setUpClass()
+
+        cls.batch_size = 16
+        cls.num_actions = 3
+        cls.input_size = 5
+        cls.temporal_size = 7
+        cls.T = 10
+
+        cls.states = np.random.random((cls.batch_size, cls.input_size))
+        cls.temporal_states = np.random.random((cls.batch_size, cls.T, cls.temporal_size))
+        cls.advantage = np.random.random((cls.batch_size, )).astype('float32')
+        cls.actions = np.random.random((cls.batch_size, cls.num_actions)).astype('float32')
+        cls.discrete_actions = np.random.randint(0, 3, size=(cls.batch_size, )).astype('int32')
+
+    def learn_policy_test(self):
+
+        estimator = DiscreteAndContPolicyEstimator(
+            self.num_actions, static_size=self.input_size, temporal_size=self.temporal_size,
+            shared_layer=lambda x: rnn_graph_lstm(x, 32, 1, True),
+            learning_rate=1e-2
+        )
+
+        grads = [g for g, _ in estimator.grads_and_vars]
+
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            # Run feeds
+            for _ in xrange(100):
+                feed_dict = {
+                    estimator.states: self.states,
+                    estimator.history: self.temporal_states,
+                    estimator.advantages: np.ones_like(self.advantage),
+                    estimator.actions: self.actions[:, 0],
+                    estimator.discrete_actions: tf.keras.utils.to_categorical(self.discrete_actions, num_classes=3).astype('float32')
+                }
+                pred = sess.run(estimator.predictions, feed_dict)
+
+                grads_ = sess.run(grads, feed_dict)
+
+                grad_feed_dict = { k: v for k, v in zip(grads, grads_) }
+                _ = sess.run(estimator.train_op, grad_feed_dict)
+
+        prob_optimal_choice = pred['probs'][np.arange(self.batch_size), self.discrete_actions]
+        cont_action_optimal_choice = pred['mu'][np.arange(self.batch_size), self.discrete_actions]
+        np.testing.assert_array_less(0.9, prob_optimal_choice)
+        self.assertLess(np.mean(np.abs((cont_action_optimal_choice - self.actions[:, 0]))), 0.2)
+
+    def predict_test(self):
+        estimator = DiscreteAndContPolicyEstimator(
+            self.num_actions, static_size=self.input_size, temporal_size=self.temporal_size,
+            shared_layer=lambda x: rnn_graph_lstm(x, 32, 1, True)
+        )
+
+        grads = [g for g, _ in estimator.grads_and_vars]
+
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            # Run feeds
+            losses = []
+            for _ in xrange(10):
+                feed_dict = {
+                    estimator.states: self.states,
+                    estimator.history: self.temporal_states,
+                    estimator.advantages: self.advantage,
+                    estimator.actions: self.actions[:, 0],
+                    estimator.discrete_actions: tf.keras.utils.to_categorical(self.discrete_actions, num_classes=3).astype('float32')
+                }
+                loss = sess.run(estimator.loss, feed_dict)
+                losses.append(loss)
+                pred = sess.run(estimator.predictions, feed_dict)
+
+                grads_ = sess.run(grads, feed_dict)
+
+                grad_feed_dict = { k: v for k, v in zip(grads, grads_) }
+                _ = sess.run(estimator.train_op, grad_feed_dict)
+
+            # Assertions
+            self.assertLess(losses[-1], losses[0])
+            np.testing.assert_array_less(0., pred['sigma'])
+            self.assertEqual(pred['probs'].shape[0], self.batch_size)
+            self.assertEqual(pred['probs'].shape[1], self.num_actions)
+            self.assertEqual(pred['mu'].shape[1], self.num_actions)
+            self.assertEqual(pred['sigma'].shape[1], self.num_actions)
 
 
 class PolicyEstimatorTest(tf.test.TestCase):
@@ -49,7 +140,7 @@ class PolicyEstimatorTest(tf.test.TestCase):
                 grad_feed_dict = { k: v for k, v in zip(grads, grads_) }
                 _ = sess.run(estimator.train_op, grad_feed_dict)
 
-        self.assertLess(np.mean(np.abs((pred['mu'] - self.actions))), 0.2)
+        self.assertLess(np.mean(np.abs((pred['mu'] - self.actions))), 0.3)
 
     def gaussian_predict_test(self):
         estimator = GaussianPolicyEstimator(
